e34aecff79cb799e5313e61573582d0ad1ea1800
jt-koodi <koulusivu@mailo.com>
2026-02-25 19:41:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const backToTopButton = document.querySelector('.back-to-top');
 window.addEventListener('scroll', () => {
     // Näytetään nappi, kun on skrollattu yli n pikseliä
-    if (window.scrollY > 600 || window.pageYOffset > 600) {
+    if (window.scrollY > 400 || window.pageYOffset > 400) {
         backToTopButton.classList.add('show');
     } else {
         backToTopButton.classList.remove('show');
